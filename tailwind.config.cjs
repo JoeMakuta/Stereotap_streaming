8d0ac10ed5e7e5474f915cc3f5034d414809049f
Josue Makuta <makutajosue@gmail.com>
2022-09-06 16:04:23 +0200

    Feat : Display of  Artists done!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,11 +13,13 @@ module.exports = {
         'mainBackground': "url('./src/Asserts/pexels-gustavo-fring-4127623.jpg')",
         'subMainBackground': "url('./src/Asserts/pexels-andrea-piacquadio-783243.jpg')",
         'thirdBackground': "url('./src/Asserts/pexels-orione-conceição-8663209.jpg')",
-        
+
       },
       colors: {
         'mainColor': '#2F3444',
-        'backOpacity': 'rgba(255,255,255,0.3)'
+        'backOpacity': 'rgba(255,255,255,0.3)',
+        'blackOpacity': 'rgba(0,0,0,0.5)'
+
       },
 
     },
